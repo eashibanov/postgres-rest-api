b264e3f36e310b6854c209b2efa70447009b783a
Egor Shibanov <eashibanov@edu.hse.ru>
2021-01-14 10:22:07 +0300

    update

--- database/1-init-db.sql
+++ database/1-init-db.sql
@@ -1,19 +1,27 @@
-CREATE USER postgres;
+--CREATE USER postgres;
 CREATE DATABASE phonebook;
-GRANT ALL PRIVILEGES ON DATABASE docker TO phonebook;
+--GRANT ALL PRIVILEGES ON DATABASE docker TO phonebook;
 
-\l
+--\l
 
 \c phonebook;
+
+CREATE USER api WITH PASSWORD 'apipassword';
+
+GRANT CONNECT ON DATABASE "phonebook" TO api;
+
+GRANT ALL PRIVILEGES ON DATABASE "phonebook" TO api;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO api;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO api;
 
 CREATE TABLE people (
     id SERIAL PRIMARY KEY,
     name VARCHAR(40),
-    phone CHAR(14)
+    phone CHAR(15)
 );
 
-INSERT INTO users (name, phone)
+INSERT INTO people (name, phone)
     VALUES ('joe', '1-800-555-35-35'),
     ('ryan', '1-800-999-99-99');
 
-select * from users;
+select * from people;
